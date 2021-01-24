61a3f51d4351a27a4b66dd51ffca55ec8bbdfe22
golima <lima01@gmail.com>
2021-01-24 12:50:15 -0500

    Convert switch/case to if/else

--- src/com/company/WeekdayInEnglish.java
+++ src/com/company/WeekdayInEnglish.java
@@ -9,31 +9,30 @@ public class WeekdayInEnglish {
     int dayOfWeek = Integer.parseInt(strDay);
 
     String day;
-    switch (dayOfWeek) {
-      case 1:
-        day = "Monday";
-        break;
-      case 2:
-        day = "Tuesday";
-        break;
-      case 3:
-        day = "Wednesday";
-        break;
-      case 4:
-        day = "Thursday";
-        break;
-      case 5:
-        day = "Friday";
-        break;
-      case 6:
-        day = "Saturday";
-        break;
-      case 7:
-        day = "Sunday";
-        break;
-      default:
-        System.out.println("Day of week must be between 1 and 7.");
-        return;
+    if( dayOfWeek == 1 ) {
+      day = "Monday";
+    } 
+    else if ( dayOfWeek == 2 ) {
+      day = "Tuesday";
+      }
+    else if ( dayOfWeek == 3 ) {
+      day = "Wednesday";
+      }
+    else if ( dayOfWeek == 4 ) {
+      day = "Thursday";
+      }
+    else if ( dayOfWeek == 5 ) {
+      day = "Friday";
+      }
+    else if ( dayOfWeek == 6 ) {
+      day = "Saturday";
+      }
+    else if ( dayOfWeek == 7 ) {
+      day = "Sunday";
+      }
+    else {
+      System.out.println("Day of week must be between 1 and 7.");
+      return;
     }
     System.out.println("The " + dayOfWeek + " day of week is:" + day + ".");
   }
